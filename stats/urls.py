9e48635c5016ebaece9c13e3dd8a8a96cc4f7e3d
Krishna Tulsyan <kt.krishna.tulsyan@gmail.com>
2023-11-23 11:42:51 +0530

    added qc stats

--- stats/urls.py
+++ stats/urls.py
@@ -6,4 +6,5 @@ app_name = 'stats'
 urlpatterns = [
 	path('user/', views.UserStatsView.as_view(), name='user'),
 	path('language/', views.LanguageStatsView.as_view(), name='language'),
+	path('qc/', views.QCStatsView.as_view(), name='qc'),
 ]
--- stats/views.py
+++ stats/views.py
@@ -24,6 +24,32 @@ class BaseStatsView(LoginRequiredMixin, UserPassesTestMixin):
 		return ret
 
 
+class QCStatsView(BaseStatsView, TemplateView):
+	template_name = 'stats/qc.html'
+
+	def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+		category = self.request.GET.get('category', '')
+		ret = Page.objects.all()
+		if category:
+			ret = ret.filter(category=category)
+		ret = ret.values('language').annotate(
+			name=F('language'),
+			total_count=Count('id'),
+			approved_count=Count('id', filter=Q(qc_status='approved')),
+			rejected_count=Count('id', filter=Q(qc_status='rejected')),
+			# **self.create_count_annotations([
+			# 	i[0] for i in Page.STATUS_CHOICES
+			# ])
+		)
+		kwargs.update({
+			'language_list': ret,
+			'category': category,
+			'category_list': Page.get_all_categories(),
+		})
+		print(kwargs)
+		return super().get_context_data(**kwargs)
+
+
 class UserStatsView(BaseStatsView, TemplateView):
 	template_name = 'stats/user.html'
 
